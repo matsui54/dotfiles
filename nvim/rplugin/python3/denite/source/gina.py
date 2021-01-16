215664003112c5d1876cef617f9ce824a2b7c143
matsui54 <haru.matu9168@gmail.com>
2021-01-16 19:23:47 +0900

    add gina denite-source

---
+++ nvim/rplugin/python3/denite/source/gina.py
@@ -0,0 +1,28 @@
+import pathlib
+
+from denite.base.source import Base
+from denite.util import globruntime, Nvim, UserContext, Candidates
+
+
+class Source(Base):
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
+        self.vim = vim
+        self.name = "gina"
+        self.kind = "command"
+
+    def gather_candidates(self, context):
+        candidates = []
+        suffix = 'autoload/gina/command/*.vim'
+        command_names = []
+        for rtp in self.vim.options['runtimepath'].split(','):
+            paths = pathlib.Path(rtp).glob(suffix)
+            command_names += [i.stem for i in paths]
+        for name in list(filter(lambda x: x[0] != '_', command_names)):
+            candidates.append(
+                {
+                    "word": "{}".format(name),
+                    "action__command": "Gina {}".format(name),
+                }
+            )
+        return candidates
