eff1eb7808769801f873b7b819f7e0462449299a
matsui54 <haru.matu9168@gmail.com>
2021-03-14 22:44:02 +0900

    update

--- nvim/rplugin/python3/denite/source/git/log.py
+++ nvim/rplugin/python3/denite/source/git/log.py
@@ -47,8 +47,12 @@ class Source(Base):
 class Kind(KindBase):
     def __init__(self, vim: Nvim) -> None:
         super().__init__(vim)
-
         self.name = 'gitdiff'
+        self.default_action = 'open'
+
+    def action_open(self, context: UserContext) -> None:
+        target = context['targets'][0]
+        self.vim.command("Gina show {}".format(target['__obj']))
 
     def action_preview(self, context: UserContext) -> None:
         target = context['targets'][0]
