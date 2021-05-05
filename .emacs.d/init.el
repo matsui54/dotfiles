51388dcbe0c2a813e138d447872c91f251fadb8a
matsui54 <haru.matu9168@gmail.com>
2021-05-05 22:27:49 +0900

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm auto-complete auto-compile magit))))
+ '(package-selected-packages
+   (quote
+    (ddskk evil smooth-scroll helm auto-complete auto-compile magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,7 +16,16 @@
  ;; If there is more than one, they won't work right.
  )
 
+(global-set-key (kbd "C-x C-j") 'skk-mode)
+(global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\C-xh" 'help-command) ; override mark-whole-buffer
 (setq ring-bell-function 'ignore)
 
 (require 'helm-config)
 (helm-mode 1)
+
+(require 'evil)
+(evil-mode 1)
+
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
