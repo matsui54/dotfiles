766d113dea660f982c3ee5df8f51eb9559596700
matsui54 <haru.matu9168@gmail.com>
2022-02-27 18:08:18 +0900

    Add highlights

--- nvim/denops/@ddu-filters/matcher_fzy.ts
+++ nvim/denops/@ddu-filters/matcher_fzy.ts
@@ -6,64 +6,55 @@ import {
 import {
   BaseFilter,
   DduItem,
-  SourceOptions,
-} from "https://deno.land/x/ddu_vim@v0.1.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v0.1.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v0.14/types.ts#^";
+import { Denops } from "https://deno.land/x/ddu_vim@v0.14/deps.ts#^";
 
-type Params = Record<never, never>;
+type Params = {
+  hlGroup: string;
+};
 
-/*
-var query = "amuser";
-
-// fzy.js includes `hasMatch` which can be used for filtering
-list = list.filter((s) => fzy.hasMatch(s));
-
-// Sort by fzy's scoring, descending (higher scores are better matches)
-list = sortBy(list, (s) => -fzy.score(query, s));
-
-// Select only the first 10 results
-list.slice(0, 10);
-
-// Print out our results with matched positions
-list.forEach((s) => {
-	var padded = "";
-	var p = fzy.positions(query, s);
-	for(var i = 0; i < query.length; i++) {
-		padded = padded.padEnd(p[i], ' ') + query[i];
-	}
-
-	console.log(s);
-	console.log(padded);
-	console.log("");
-});
-*/
 type SortItem = {
   score: number;
   item: DduItem;
 };
 
 export class Filter extends BaseFilter<Params> {
-  private cache: Record<string, DduItem>;
   filter(args: {
     denops: Denops;
-    sourceOptions: SourceOptions;
     input: string;
     items: DduItem[];
+    filterParams: Params;
   }): Promise<DduItem[]> {
     const input = args.input;
     const filtered = args.items.filter((i) => hasMatch(input, i.word));
-    if (filtered.length > 100000) {
+    if (filtered.length > 10000) {
       return Promise.resolve(filtered);
     }
     return Promise.resolve(
       filtered.map((
         c,
       ) => ({ score: score(input, c.word), item: c } as SortItem))
-        .sort((a, b) => b.score - a.score).map((c) => c.item),
+        .sort((a, b) => b.score - a.score).map((c) => {
+          const highlights = positions(input, c.item.word).map((p) => ({
+            col: p + 1,
+            type: "abbr",
+            name: "ddu_fzy_hl",
+            "hl_group": args.filterParams.hlGroup,
+            width: 1,
+          }));
+          if (c.item.highlights) {
+            c.item.highlights = c.item.highlights.concat(highlights);
+          } else {
+            c.item.highlights = highlights;
+          }
+          return c.item;
+        }),
     );
   }
 
   params(): Params {
-    return {};
+    return {
+      hlGroup: "Title",
+    };
   }
 }
