11043b2a827d6ebfd57c86804b1fafea334cca94
matsui54 <haru.matu9168@gmail.com>
2022-03-31 13:42:08 +0900

    Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("Cica"),
   font_size = 14.0,
-  hide_tab_bar_if_only_one_tab = true,
   color_scheme = "Terminal Basic",
   use_ime = true,
+  window_decorations = "RESIZE"
 }
