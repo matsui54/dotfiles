0cfe7213ad7a36497df4cce6d6ec3bdb05639106
matsui54 <haru.matu9168@gmail.com>
2022-03-31 18:14:51 +0900

    wezterm: fix window padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,5 +4,11 @@ return {
   font_size = 14.0,
   color_scheme = "Terminal Basic",
   use_ime = true,
-  window_decorations = "RESIZE"
+  window_decorations = "RESIZE",
+  window_padding = {
+    left = 4,
+    right = 4,
+    top = 0,
+    bottom = 0,
+  },
 }
