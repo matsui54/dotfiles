9d18bf5f2616679083f686bfabcbbef07709afd3
matsui54 <haru.matu9168@gmail.com>
2022-04-01 23:16:50 +0900

    Load local config file for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,26 @@
 local wezterm = require 'wezterm';
-return {
+
+local function load_local_config()
+	local ok, _ = pcall(require, "local")
+	if not ok then
+		return {}
+	end
+	return require("local").setup()
+end
+local local_config = load_local_config()
+
+local function merge_tables(t1, t2)
+	for k, v in pairs(t2) do
+		if (type(v) == "table") and (type(t1[k] or false) == "table") then
+			M.merge_tables(t1[k], t2[k])
+		else
+			t1[k] = v
+		end
+	end
+	return t1
+end
+
+local config = {
   font = wezterm.font("Cica"),
   font_size = 14.0,
   color_scheme = "Terminal Basic",
@@ -12,3 +33,5 @@ return {
     bottom = 0,
   },
 }
+
+return merge_tables(config, local_config)
