e6c744715bdbe49b78c61fc5f7e6c51dc852f8f5
matsui54 <haru.matu9168@gmail.com>
2022-04-25 16:46:05 +0900

    Update lsp config

--- nvim/lua/lsp_rc.lua
+++ nvim/lua/lsp_rc.lua
@@ -22,6 +22,7 @@ local on_attach = function(client)
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = false,
+      severity_sort = true,
     }
   )
   -- require "lsp_signature".on_attach({
