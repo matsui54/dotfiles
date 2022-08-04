fe882f1dba05e77d1bb8f817ac2423dee9aa335c
matsui54 <haru.matu9168@gmail.com>
2022-08-04 22:08:55 +0900

    Add dap mappings

--- nvim/lua/dap_rc.lua
+++ nvim/lua/dap_rc.lua
@@ -43,3 +43,9 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+vim.keymap.set('n', '<F5>', function() require 'dap'.continue() end)
+vim.keymap.set('n', '<F9>', function() require 'dap'.step_into() end)
+vim.keymap.set('n', '<F10>', function() require 'dap'.step_over() end)
+vim.keymap.set('n', '<F12>', function() require 'dap'.step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require 'dap'.toggle_breakpoint() end)
