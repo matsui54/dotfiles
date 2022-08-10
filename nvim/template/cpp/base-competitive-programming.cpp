c9371146cdf94bbddf0764f31d2eb4c39cfbe189
matsui54 <haru.matu9168@gmail.com>
2022-08-10 23:29:52 +0900

    Add template

---
+++ nvim/template/cpp/base-competitive-programming.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		{{_cursor_}}
+	}
+	return 0;
+}
