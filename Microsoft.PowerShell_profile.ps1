f1592866b6b5e28343e17d352f530f03110eda09
matsui54 <haru.matu9168@gmail.com>
2022-11-24 00:53:20 +0900

    Update PowerShell_profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,40 @@
 Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteChar
 Set-PSReadLineOption -PredictionSource None
 
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Syntax highlighting colors
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$is_admin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-Set-PSReadLineOption -Colors @{
-  Command            = 'Black'
-  Number             = 'DarkGray'
-  Member             = 'DarkGray'
-  Operator           = 'DarkGray'
-  Type               = 'DarkGray'
-  Variable           = 'DarkGreen'
-  Parameter          = 'DarkGreen'
-  ContinuationPrompt = 'DarkGray'
-  Default            = 'DarkGray'
+if (!$is_admin) {
+  # Syntax highlighting colors
+  Set-PSReadLineOption -Colors @{
+    Command            = 'Black'
+    Number             = 'DarkGray'
+    Member             = 'DarkGray'
+    Operator           = 'DarkGray'
+    Type               = 'DarkGray'
+    Variable           = 'DarkGreen'
+    Parameter          = 'DarkGreen'
+    ContinuationPrompt = 'DarkGray'
+    Default            = 'DarkGray'
+  }
+   
+  # Logging / Progress colors
+  $p = $host.privatedata
+  $p.ErrorForegroundColor    = "Red"
+  $p.ErrorBackgroundColor    = "White"
+  $p.WarningForegroundColor  = "Yellow"
+  $p.WarningBackgroundColor  = "White"
+  $p.DebugForegroundColor    = "Yellow"
+  $p.DebugBackgroundColor    = "White"
+  $p.VerboseForegroundColor  = "Black"
+  $p.VerboseBackgroundColor  = "White"
+  $p.ProgressForegroundColor = "DarkGray"
+  $p.ProgressBackgroundColor = "White"
+
+  $env:BAT_THEME = 'GitHub'
+  $env:FZF_DEFAULT_OPTS = "--color=light"
 }
- 
-# Logging / Progress colors
- 
-$p = $host.privatedata
-$p.ErrorForegroundColor    = "Red"
-$p.ErrorBackgroundColor    = "White"
-$p.WarningForegroundColor  = "Yellow"
-$p.WarningBackgroundColor  = "White"
-$p.DebugForegroundColor    = "Yellow"
-$p.DebugBackgroundColor    = "White"
-$p.VerboseForegroundColor  = "Black"
-$p.VerboseBackgroundColor  = "White"
-$p.ProgressForegroundColor = "DarkGray"
-$p.ProgressBackgroundColor = "White"
