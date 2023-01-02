027c3405737bfaa356008083ee1b9f82499d10b8
matsui54 <haru.matu9168@gmail.com>
2023-01-03 00:24:02 +0900

    Try neodev

--- nvim/lua/lsp_rc.lua
+++ nvim/lua/lsp_rc.lua
@@ -59,6 +59,8 @@ local on_attach = function(client)
   end
 end
 
+require("neodev").setup()
+
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.preselectSupport = false
