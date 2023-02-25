de0513181a1fab9d78a70f350b2d2fae3c23360a
matsui54 <haru.matu9168@gmail.com>
2023-02-25 09:27:15 +0900

    Write ddu config in typescript

--- nvim/denops/vimrc/plugins/ddu.ts
+++ nvim/denops/vimrc/plugins/ddu.ts
@@ -1,12 +1,47 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.12.1/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v3.12.1/batch/mod.ts";
+import {
+  map,
+  MapOptions,
+} from "https://deno.land/x/denops_std@v4.0.0/mapping/mod.ts";
+// import * as op from "https://deno.land/x/denops_std@v4.0.0/option/mod.ts";
+// import * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
+import * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
 import { DduOptions } from "https://deno.land/x/ddu_vim@v2.2.0/types.ts";
+import {
+  Fn,
+  register,
+} from "https://deno.land/x/denops_std@v4.0.0/lambda/mod.ts";
 
 export function main(denops: Denops) {
   denops.dispatcher = {
     ...denops.dispatcher,
     async ddu_setup(): Promise<void> {
       await batch(denops, async (denops) => {
+        const m = async (lhs: string, rhs: string, op?: MapOptions) => {
+          await map(denops, lhs, rhs, op);
+        };
+        await m("<Space>d", "<cmd>Ddu source<CR>");
+        await m("<Space>a", "<cmd>Ddu file_external<CR>");
+        await m("<Space>h", "<cmd>Ddu help<CR>");
+        await m("<Space>o", "<cmd>Ddu file_old<CR>");
+        await m("<Space>s", "<cmd>Ddu directory_rec<CR>");
+        await m("<Space>n", "<cmd>Ddu ghq<CR>");
+        await m("<Space>b", "<cmd>Ddu buffer<CR>");
+        await m("<Space>r", "<cmd>Ddu -resume<CR>");
+        await m("<Space>g", "<cmd>DduRgLive<CR>");
+        await m("<Space>m", "<cmd>Ddu man<CR>");
+        await m(
+          "<Space>f",
+          "<cmd>Ddu file_external -source-option-path=~/dotfiles<CR>",
+        );
+        await m("g0", "<cmd>LspDocumentSymbols<CR>");
+        await m(
+          "<C-t>",
+          `"<C-u><ESC><cmd>Ddu command_history -input='" . getcmdline() . "'<CR>"`,
+          { mode: "c", expr: true, silent: true },
+        );
+
         const patchGlobal = async (dict: Partial<DduOptions>) => {
           await denops.call("ddu#custom#patch_global", dict);
         };
@@ -18,87 +53,44 @@ export function main(denops: Denops) {
         );
         await denops.call("ddu#custom#alias", "source", "ghq", "file_external");
         await patchGlobal({
-          "ui": "ff",
-          "profile": false,
-          "sourceOptions": {
-            "_": {
-              "ignoreCase": true,
-              "matchers": ["matcher_fzy"],
-            },
-            "dein": {
-              "defaultAction": "cd",
-            },
-            "line": {
-              "matchers": ["matcher_kensaku"],
-            },
-            "highlight": {
-              "defaultAction": "edit",
-            },
-            "help": {
-              "defaultAction": "open",
-            },
-            "file_external": {
-              "defaultAction": "open",
-            },
-            "directory_rec": {
-              "defaultAction": "cd",
-            },
-            "man": {
-              "defaultAction": "open",
-            },
-            "ghq": {
-              "defaultAction": "cd",
-              "path": "~",
-            },
-            "command_history": {
-              "defaultAction": "execute",
-            },
-            "dein_update": {
-              "matchers": ["matcher_dein_update"],
-            },
-            "nvim_lsp_document_symbol": {
-              "columns": ["lsp_symbols"],
-            },
+          ui: "ff",
+          profile: false,
+          sourceOptions: {
+            _: { ignoreCase: true, matchers: ["matcher_fzy"] },
+            dein: { defaultAction: "cd" },
+            line: { matchers: ["matcher_kensaku"] },
+            highlight: { defaultAction: "edit" },
+            help: { defaultAction: "open" },
+            file_external: { defaultAction: "open" },
+            directory_rec: { defaultAction: "cd" },
+            man: { defaultAction: "open" },
+            ghq: { defaultAction: "cd", path: "~" },
+            command_history: { defaultAction: "execute" },
+            dein_update: { matchers: ["matcher_dein_update"] },
+            nvim_lsp_document_symbol: { columns: ["lsp_symbols"] },
           },
-          "kindOptions": {
-            "file": {
-              "defaultAction": "open",
-            },
-            "action": {
-              "defaultAction": "do",
-            },
-            "dein_update": {
-              "defaultAction": "viewDiff",
-            },
-            "gin_action": {
-              "defaultAction": "execute",
-            },
-            "source": {
-              "defaultAction": "execute",
-            },
-            "ui_select": {
-              "defaultAction": "select",
-            },
+          kindOptions: {
+            file: { defaultAction: "open" },
+            action: { defaultAction: "do" },
+            dein_update: { defaultAction: "viewDiff" },
+            gin_action: { defaultAction: "execute" },
+            source: { defaultAction: "execute" },
+            ui_select: { defaultAction: "select" },
           },
-          "actionOptions": {
-            "echo": {
-              "quit": false,
-            },
-            "echoDiff": {
-              "quit": false,
-            },
+          actionOptions: {
+            echo: { quit: false },
+            echoDiff: { quit: false },
+            newFile: { quit: false },
+            newDirectory: { quit: false },
+            narrow: { quit: false },
           },
-          "filterParams": {
-            "matcher_fzy": {
-              "hlGroup": "Special",
-            },
-            "matcher_kensaku": {
-              "highlightMatched": "Search",
-            },
+          filterParams: {
+            matcher_fzy: { hlGroup: "Special" },
+            matcher_kensaku: { highlightMatched: "Search" },
           },
-          "sourceParams": {
-            "file_external": {
-              "cmd": [
+          sourceParams: {
+            file_external: {
+              cmd: [
                 "fd",
                 ".",
                 "-H",
@@ -110,8 +102,8 @@ export function main(denops: Denops) {
                 "f",
               ],
             },
-            "directory_rec": {
-              "cmd": [
+            directory_rec: {
+              cmd: [
                 "fd",
                 ".",
                 "-H",
@@ -123,66 +115,95 @@ export function main(denops: Denops) {
                 "d",
               ],
             },
-            "rg": {
-              "args": ["--json", "--ignore-case"],
-              "highlights": {
-                "path": "SpecialComment",
-                "lineNr": "LineNr",
-                "word": "Constant",
+            rg: {
+              args: ["--json", "--ignore-case"],
+              highlights: {
+                path: "SpecialComment",
+                lineNr: "LineNr",
+                word: "Constant",
               },
             },
-            "ghq": { "cmd": ["ghq", "list", "-p"] },
+            ghq: { cmd: ["ghq", "list", "-p"] },
           },
-          "uiParams": {
-            "ff": {
-              "split": denops.meta.host == "nvim" ? "floating" : "horizontal",
-              "filterSplitDirection": "floating",
-              "filterFloatingPosition": "top",
-              "ignoreEmpty": true,
-              "autoResize": true,
+          uiParams: {
+            ff: {
+              split: denops.meta.host == "nvim" ? "floating" : "horizontal",
+              filterSplitDirection: "floating",
+              filterFloatingPosition: "top",
+              ignoreEmpty: true,
+              autoResize: true,
             },
-            "filer": {
-              "split": "no",
-            },
+            filer: { split: "no" },
           },
-          "columnParams": {
-            "lsp_symbols": {
-              "collapsedIcon": "",
-              "expandedIcon": "",
-              "iconWidth": 4,
-              "kindLabels": {
-                "File": "",
-                "Module": "",
-                "Namespace": "",
-                "Class": ["", "Special"],
-                "Method": ["", "Special"],
-                "Text": "",
-                "Function": ["", "Special"],
-                "Constructor": ["", "Special"],
-                "Field": "",
-                "Variable": "",
-                "Interface": "",
-                "Property": "",
-                "Unit": "",
-                "Value": "",
-                "Enum": "",
-                "Keyword": "",
-                "Snippet": "",
-                "Color": "",
-                "Reference": "",
-                "Folder": "",
-                "Constant": "",
-                "EnumMember": "",
-                "Struct": "",
-                "Event": "",
-                "Operator": "",
-                "TypeParameter": "",
+          columnParams: {
+            lsp_symbols: {
+              collapsedIcon: "",
+              expandedIcon: "",
+              iconWidth: 4,
+              kindLabels: {
+                File: "",
+                Module: "",
+                Namespace: "",
+                Class: ["", "Special"],
+                Method: ["", "Special"],
+                Text: "",
+                Function: ["", "Special"],
+                Constructor: ["", "Special"],
+                Field: "",
+                Variable: "",
+                Interface: "",
+                Property: "",
+                Unit: "",
+                Value: "",
+                Enum: "",
+                Keyword: "",
+                Snippet: "",
+                Color: "",
+                Reference: "",
+                Folder: "",
+                Constant: "",
+                EnumMember: "",
+                Struct: "",
+                Event: "",
+                Operator: "",
+                TypeParameter: "",
               },
+            },
+          },
+        });
+      });
+      const registerCommand = async (command: string, fn: Fn) => {
+        const id = register(denops, fn);
+        await denops.cmd(
+          `command! ${command} call denops#request('vimrc', '${id}', [])`,
+        );
+      };
+      await registerCommand("DduFiler", async () => {
+        const start = async (dict: Partial<DduOptions>) => {
+          await denops.call("ddu#start", dict);
+        };
+        await start({
+          ui: "filer",
+          // name: 'filer-' + await fn.(),
+          resume: true,
+          sync: true,
+          sources: [{
+            name: "file",
+            options: {
+              path: await vars.t.get(denops, "ddu_ui_filer_path", ""),
+              columns: ["filename"],
+            },
+          }],
+          actionOptions: {
+            narrow: {
+              quit: false,
             },
           },
         });
       });
       return Promise.resolve();
     },
+    // async ddu_filer(): Promise<void> {
+    // },
   };
 }
