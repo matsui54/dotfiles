2b62554fb123ad446f3fead0fac6cc7f2b1477c7
matsui54 <haru.matu9168@gmail.com>
2023-05-09 15:45:11 +0900

    Update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,18 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-polybar &
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
-echo "Bars launched..."
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        MONITOR=$m TRAY_POS=right polybar main &
+    else
+        MONITOR=$m TRAY_POS=none polybar secondary &
+    fi
+  done
+fi
