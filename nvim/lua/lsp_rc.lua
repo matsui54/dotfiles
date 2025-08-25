b6a1d5f0e589ffa74e84ed64f66a92f5a3862023
matsui54 <haru.matu9168@gmail.com>
2025-08-25 21:27:25 +0900

    Update lsp

--- nvim/lua/lsp_rc.lua
+++ nvim/lua/lsp_rc.lua
@@ -70,76 +70,69 @@ local buf_name = vim.api.nvim_buf_get_name(0)
 local current_buf = vim.api.nvim_get_current_buf()
 local is_node_repo = node_root_dir(buf_name, current_buf) ~= nil
 
--- nvim_lsp.clangd.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   cmd = {
---     "clangd",
---     "--offset-encoding=utf-16",
---   },
--- }
--- nvim_lsp.gopls.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.vimls.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.pyright.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.julials.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.bashls.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.svlangserver.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   settings = {
---     systemverilog = {
---       launchConfiguration = "verilator -sv -Wall --lint-only pkg_def.sv",
---     }
---   }
--- }
--- nvim_lsp.svls.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.denols.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   init_options = {
---     lint = true,
---     unstable = true,
---   },
---   autostart = not (is_node_repo),
--- }
--- nvim_lsp.lua_ls.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   settings = {
---     Lua = {
---       runtime = {
---         version = 'LuaJIT',
---         path = vim.split(package.path, ';'),
---       },
---       diagnostics = {
---         globals = { 'vim' },
---       },
---       workspace = {
---         library = {
---           [vim.fn.expand('$VIMRUNTIME/lua')] = true,
---           [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
---         },
---       },
---     },
---   }
--- }
--- nvim_lsp.rust_analyzer.setup { on_attach = on_attach, capabilities = capabilities }
--- nvim_lsp.ts_ls.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   autostart = is_node_repo,
--- }
--- nvim_lsp.efm.setup {
---   init_options = { documentFormatting = true },
---   settings = {
---     rootMarkers = { ".git/" },
---     languages = {
---       markdown = {
---         { formatCommand = "deno fmt - --ext md --line-width 200", formatStdin = true }
---       },
---       python = {
---         { formatCommand = "black --quiet -", formatStdin = true }
---       }
---     }
---   }
--- }
+vim.lsp.config('*', {
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+vim.lsp.config('svlangserver', {
+  settings = {
+    systemverilog = {
+      launchConfiguration = "verilator -sv -Wall --lint-only pkg_def.sv",
+    }
+  }
+})
+vim.lsp.config('texlab', {
+  root_dir = nvim_lsp.util.root_pattern('main.tex'),
+  settings = {
+    texlab = {
+      rootDirectory = ".",
+    }
+  }
+})
+vim.lsp.config('zls', {
+  cmd = { vim.fn.expand('$HOME/ghq/github.com/zigtools/zls/zig-out/bin/zls') },
+})
+vim.lsp.config('denols', {
+  init_options = {
+    lint = true,
+    unstable = true,
+  },
+  autostart = not (is_node_repo),
+})
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  }
+})
+vim.lsp.config('ts_ls', {
+  autostart = is_node_repo,
+})
+vim.lsp.config('efm', {
+  init_options = { documentFormatting = true },
+  settings = {
+    rootMarkers = { ".git/" },
+    languages = {
+      markdown = {
+        { formatCommand = "deno fmt - --ext md --line-width 200", formatStdin = true }
+      },
+      python = {
+        { formatCommand = "black --quiet -", formatStdin = true }
+      }
+    }
+  }
+})
+vim.lsp.enable({ 'pyright' })
