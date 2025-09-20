99d7d6189c35cbc46f4208f4bdab054008be31ce
matsui54 <haru.matu9168@gmail.com>
2025-09-20 16:41:48 +0900

    Remove unnecessary lsp config

--- nvim/lua/lsp_rc.lua
+++ nvim/lua/lsp_rc.lua
@@ -66,12 +66,6 @@ vim.diagnostic.config({
 local capabilities = require(
   "ddc_source_lsp"
 ).make_client_capabilities()
-local nvim_lsp = require('lspconfig')
-
-local node_root_dir = nvim_lsp.util.root_pattern("package.json", "node_modules")
-local buf_name = vim.api.nvim_buf_get_name(0)
-local current_buf = vim.api.nvim_get_current_buf()
-local is_node_repo = node_root_dir(buf_name, current_buf) ~= nil
 
 vim.lsp.config('*', {
   capabilities = capabilities,
@@ -80,60 +74,6 @@ vim.lsp.config('svlangserver', {
   settings = {
     systemverilog = {
       launchConfiguration = "verilator -sv -Wall --lint-only pkg_def.sv",
-    }
-  }
-})
-vim.lsp.config('texlab', {
-  root_dir = nvim_lsp.util.root_pattern('main.tex'),
-  settings = {
-    texlab = {
-      rootDirectory = ".",
-    }
-  }
-})
-vim.lsp.config('zls', {
-  cmd = { vim.fn.expand('$HOME/ghq/github.com/zigtools/zls/zig-out/bin/zls') },
-})
-vim.lsp.config('denols', {
-  init_options = {
-    lint = true,
-    unstable = true,
-  },
-  autostart = not (is_node_repo),
-})
-vim.lsp.config('lua_ls', {
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-    },
-  }
-})
-vim.lsp.config('ts_ls', {
-  autostart = is_node_repo,
-})
-vim.lsp.config('efm', {
-  init_options = { documentFormatting = true },
-  settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-      markdown = {
-        { formatCommand = "deno fmt - --ext md --line-width 200", formatStdin = true }
-      },
-      python = {
-        { formatCommand = "black --quiet -", formatStdin = true }
-      }
     }
   }
 })
